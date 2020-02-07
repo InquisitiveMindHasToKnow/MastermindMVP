535516398d37ded2c6fce85041451f8e8617af16
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2020-02-07 17:33:42 -0500

    Added all app drawables, default font, and layout for MainPageActivity.

---
+++ app/src/main/java/org/ohmstheresistance/mastermindmvp/activities/MainPageActivity.java
@@ -0,0 +1,16 @@
+package org.ohmstheresistance.mastermindmvp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.ohmstheresistance.mastermindmvp.R;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page_activity);
+    }
+}
